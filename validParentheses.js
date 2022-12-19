072a2075045469837ec7d1fcc82eb34f64be98a4
burd5 <austinburdette5@gmail.com>
2022-12-19 17:08:38 -0500

    completed challenge using stack array

---
+++ validParentheses.js
@@ -0,0 +1,39 @@
+/*
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+*/
+
+var isValid = function(s) {
+    let stack = [];
+    for(let i = 0; i < s.length; i++){
+        let current = s[i];
+        let last = stack[stack.length - 1];
+        if(current === ('(')) stack.push(')');
+        else if(current === ('[')) stack.push(']');
+        else if(current === ('{')) stack.push('}');
+        else if(current === last) stack.pop();
+        else return false;
+    }
+    return stack.length === 0;
+};
