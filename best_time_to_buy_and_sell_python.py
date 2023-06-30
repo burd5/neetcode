af5ddc2a094694b572948f554e63aa2bf3589434
burd5 <austinburdette5@gmail.com>
2023-06-30 12:15:28 -0600

    completed challenge using sliding window technique

---
+++ best_time_to_buy_and_sell_python.py
@@ -0,0 +1,14 @@
+# Best Time to Buy and Sell Stock - Neetcode
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            res = max(res, price - lowest)
+        return res
+
+
