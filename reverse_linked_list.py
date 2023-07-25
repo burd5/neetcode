21d8b37985847d7d619ff676f760616a28e2f72a
burd5 <austinburdette5@gmail.com>
2023-07-25 17:26:25 -0600

    completed linked list reversal

---
+++ reverse_linked_list.py
@@ -0,0 +1,24 @@
+# Neetcode - Reverse Linked List (Linked List - Easy)
+
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+"""
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
+
